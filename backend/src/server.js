631a823879f1611f9e54a63dfebb191c14b81c20
RATHOD KAUSHAL <rathodkaushal2001@gmail.com>
2025-07-25 15:05:57 +0530

    db models created

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,19 @@
 import express from "express"
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
-app.listen(5001, () => console.log("Server is Up and Running on Port: 5001✅"))
+app.get("/", (req, res) => res.send("Hello From server"))
+const startServer  = async () => {
+    try {
+        await connectDB()
+
+        app.listen(ENV.PORT, () => console.log("Server is Up and Running on Port:✅", ENV.PORT))
+    } catch (error) {
+        console.error("Failed to start server: ", error.message)
+        process.exit(1);
+    }
+};
+
+startServer();
